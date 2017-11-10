d88e9379b02409897f3f877012538dbe9b70a424
toadymohair <toadymohair@gmail.com>
2017-11-11 07:40:34 +0900

    ・NG理由判定処理を追加。・サブシーケンスを引数化。

--- smtp_test.sh
+++ smtp_test.sh
@@ -2,18 +2,24 @@
 
 con_smtp(){
 	stime=`date +"%Y/%m/%d %H:%M:%S.%2N"`
-	result=`(sleep 0.5;echo quit) | timeout -sKILL 0.5 telnet localhost 25 | awk '$1==220{print $1}'`
+
+	result=`(sleep 0.5;echo quit) | timeout -sKILL 0.5 telnet $1 $2 2>&1| awk '/^220/{print "OK"}/refused/{print "NG: connection refused.";exit;}'`
+
+	if [ -z "$result" ]; then
+		result="NG: timeout."
+	fi
+
 	echo $stime " , " $result | tee -a /tmp/test.txt
-	result=`(sleep 0.5;echo quit) | timeout -sKILL 0.5 telnet 10.10.10.1 25 | awk '$1==220{print $1}'`
-	#result=`(sleep 0.5;echo quit) | telnet localhost 25 | awk -v t="$stime" '$1==220{print t " , " $1}'`
-	echo $stime " , " $result | tee -a /tmp/test.txt
+
 }
+
 
 for i in {0..100}
 do
 	sleep 0.1s
-	con_smtp &
-	#echo "i = $i"	
+	con_smtp localhost 25 &
+	con_smtp localhost 24 &
+	con_smtp 10.10.10.1 25 &
 done
 
 
